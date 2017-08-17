c476c19ff3a099f8703b2c01ba2a2355b6103ca6
public-learner <sweetjustin0717@gmail.com>
2017-08-17 16:34:24 -0500

    Adds profile page and ability to update password

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,14 +1,31 @@
 angular.module('app')
-  .controller('ProfileCtrl', function($scope) {
-    this.password = '';
+  .controller('ProfileCtrl', function($scope, passwordChange) {
+    this.newPassword = '';
+
+
+    this.handlePasswordChange = (password, email) => {
+      this.error = false;
+      var obj = { password: password, email: email };
+
+      passwordChange.changePassword(obj, (err, res) => {
+        if (err) {
+          this.error = true
+          console.log(err)
+        } else {
+//          $scope.$ctrl.userId = res;
+          console.log(res);
+
+        }
+      })
+    }
+
   })
 
 .component('profilePage', {
   controller: 'ProfileCtrl',
 
   bindings: {
-    email: '=',
-    userId: '='
+    email: '='
   },
 
   
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const Promise = require('bluebird');
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -7,10 +8,9 @@ const Capsule = require('./models/capsule.js');
 const util = require('./utility.js')
 const emailService = require('./email.js');
 const cronScan = require('./cronScan.js');
-
+const hashPassword = require('./models/hashPassword.js');
 const app = express();
 
-console.log(path.join(__dirname, '../bower_components'));
 app.use('/bower_components',  express.static( path.join(__dirname, '../bower_components')));
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -214,6 +214,10 @@ app.put('/bury', (req, res) => {
         });
       }
     });
+});
+
+app.put('/passwordchange', (req, res) => {
+  hashPassword(req.body.password, req.body.email, res);
 });
 
 app.listen(3000, () => {
